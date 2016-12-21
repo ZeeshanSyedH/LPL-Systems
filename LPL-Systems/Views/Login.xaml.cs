4ba2e484c68a7ce3f031132cfb8c3459dfe0d913
Zeeshan <Zeeshan@ZSHAN>
2016-12-21 16:48:54 -0500

    Initial Commit

---
+++ LPL-Systems/Views/Login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LPL_Systems
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NavigationButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button)
+            {
+                Button pushedButton = (Button)sender;
+                switch (pushedButton.Tag.ToString())
+                {
+                    // THE TAG OF THE MINIMIZE BUTTON CONTAINS A -
+                    case "-":
+                        this.WindowState = WindowState.Minimized;
+                        break;
+                    // THE TAG OF THE EXIT BUTTON CONTAINS A x
+                    case "x":
+                        Application.Current.Shutdown();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
